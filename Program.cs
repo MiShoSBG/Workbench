ed08cd82081220605ae2420d65216027e7756a8c
MiShoSBG <47268385+MiShoSBG@users.noreply.github.com>
2019-02-02 12:20:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        static void Main(string[] args)
+        {
+           int broidni = int.Parse(Console.ReadLine());
+            int ostavenahrana = int.Parse(Console.ReadLine());
+            var cat1 = double.Parse(Console.ReadLine());
+            var cat2 = double.Parse(Console.ReadLine());
+
+            double kotka1hrana = cat1  * broidni;
+            double hrana2kotka = cat2 * broidni;
+            double sum = kotka1hrana + hrana2kotka;
+
+            if (ostavenahrana > sum)
+            {
+                Console.WriteLine("The cats are well fed;");
+                Console.WriteLine("{0} kilos of food left .", Math.Ceiling(ostavenahrana - sum));
+            }
+            else
+            {
+                Console.WriteLine("The cats are hungry.");
+                Console.WriteLine("{0} more kilos of food are needed .", Math.Floor(sum - ostavenahrana));
+                
+
+
+
+        }
+        
+    }
+}
